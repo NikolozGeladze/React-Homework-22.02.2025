ab12274f6a49da66e34b47c993c45e41a4e414d6
NikolozMokheve <158738357+NikolozGeladze@users.noreply.github.com>
2025-02-20 19:51:56 +0400

    Added Main Color To Card Info Object

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import coverImage from './assets/coverImage.svg';
 import userLogo from './assets/userLogo.jpeg';
 
 function App() {
+  var body = document.body
   var cardInfo = {
+    mainColor: '#F4D04E',
     coverImage: coverImage,
     category: 'Learning',
     publishDate: '21 Dec 2023',
@@ -13,16 +15,20 @@ function App() {
     userName: 'Nikoloz Geladze'
   }
 
+  body.style.backgroundColor = cardInfo.mainColor;
+
   return (
     <>
       <div className="card">
-        <img className="cover-img" src={cardInfo.coverImage} alt="Cover Image"/>
-        <h3 className='category'>{cardInfo.category}</h3>
+        <img className="cover-img" src={cardInfo.coverImage} alt="Cover Image" />
+        <h3 className="category" style={{ backgroundColor: cardInfo.mainColor }}>
+          {cardInfo.category}
+        </h3>
         <p className='publish-date'>Published {cardInfo.publishDate}</p>
         <h1 className='title'>{cardInfo.title}</h1>
         <p className='description'>{cardInfo.description}</p>
         <div className="user-info">
-          <img className='user-img' src={cardInfo.userLogo} alt="User Logo"/>
+          <img className='user-img' src={cardInfo.userLogo} alt="User Logo" />
           <h4 className='user-name'>{cardInfo.userName}</h4>
         </div>
       </div>
